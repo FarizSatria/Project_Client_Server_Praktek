dc1ddb5d8adcf4500162a30f07f2520fdff1698e
FarizSatria <farizsatria18@gmail.com>
2022-12-06 12:43:12 +0700

    project

--- gaji-service/src/main/java/com/fariz/gaji/service/GajiServiceApplication.java
+++ gaji-service/src/main/java/com/fariz/gaji/service/GajiServiceApplication.java
@@ -2,6 +2,8 @@ package com.fariz.gaji.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GajiServiceApplication {
@@ -9,5 +11,10 @@ public class GajiServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GajiServiceApplication.class, args);
 	}
+        
+        @Bean
+        public RestTemplate restTemplate(){
+            return new RestTemplate();
+        }
 
 }
